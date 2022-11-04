e68b2abbb5fcbd1fcc0917c1d4627488c44925ad
Barnabé Samel <barnabe.samel@gmail.com>
2022-11-04 11:52:02 +0100

    set Header to app.js

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ mongoose.connect(`mongodb+srv://hotsauceadmin:${mongomdp}@clusterhotsauces.ougjq
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use((req, res) => {
     res.json({ message: 'Votre requête a bien été reçue !' }); 
  });

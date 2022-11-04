06ea999f8e49f292388e0b84ce3fef72e79d2e94
Barnabé Samel <barnabe.samel@gmail.com>
2022-11-04 11:50:43 +0100

    connect app to mongoAtlas

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
+const dotenv = require("dotenv");
+dotenv.config();
+const mongomdp = process.env.mongomdp;
+
+mongoose.connect(`mongodb+srv://hotsauceadmin:${mongomdp}@clusterhotsauces.ougjqem.mongodb.net/?retryWrites=true&w=majority`,
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res) => {
     res.json({ message: 'Votre requête a bien été reçue !' }); 
  });
